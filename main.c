b9e1842eb2b694f041c57443a81f7d123823f03b
Nicholas Wang <me@nicho1as.wang>
2019-01-19 05:09:19 -0600

    bugfix for messing up readString and readStringW

--- main.c
+++ main.c
@@ -19,41 +19,61 @@ void sigint_handler() {
     exit(1);
 }
 
-v3f_t *recv_v3f(int *sockfd, struct sockaddr *addr) {
+v3f_t *recv_v3f(int *sockfd) {
     v3f_t *res = malloc(sizeof(v3f_t));
-    if(!res) return NULL;
+    if(!res) {
+        perror("Memory! I want more memory!");
+        return NULL;
+    }
     recv(*sockfd, &(res->x), sizeof(float), MSG_WAITALL);
     recv(*sockfd, &(res->y), sizeof(float), MSG_WAITALL);
     recv(*sockfd, &(res->z), sizeof(float), MSG_WAITALL);
     return res;
 }
 
-wchar_t *recvStringW(const int *sockfd, struct sockaddr *addr) {
-    unsigned char *buf = malloc(1);
-    struct sockaddr now_addr;
-    socklen_t this_addr_len;
-    ssize_t len = recvfrom(*sockfd, buf, 1, 0, &now_addr, &this_addr_len);
-    if(len != 1) {
+wchar_t *recvStringW(const int *sockfd) {
+    unsigned char len;
+    ssize_t l = recv(*sockfd, &len, 1, MSG_WAITALL);
+    if(l != 1) {
         perror("what?");
         return NULL;
     }
-    //if(strcmp(now_addr.sa_data, addr->sa_data) != 0)
-    //  perror("msg is from a instance that differ from the packet_id instance!\n");
-    wchar_t *msg = malloc(((unsigned) *buf + 1) * 4);
+    wchar_t *msg = malloc((size_t) (len + 1) * 4);
     if(!msg) {
-        perror("Did somebody eat all my memory?");
+        perror("Did somebody eat all of my memory?");
         return NULL;
     }
-    recvfrom(*sockfd, msg, (unsigned) *buf * 4, MSG_WAITALL, NULL, NULL);
-    msg[*buf] = 0;
+    recv(*sockfd, msg, (unsigned) len * 4, MSG_WAITALL);
+    msg[len] = 0; //in case half string.
+    return msg;
+}
+
+char *recvString(const int *sockfd) {
+    unsigned char len;
+    ssize_t l = recv(*sockfd, &len, 1, MSG_WAITALL);
+    if(l != 1) {
+        perror("what?");
+        return NULL;
+    }
+    char *msg = malloc((size_t) (len + 1));
+    if(!msg) {
+        perror("Did somebody eat all of my memory?");
+        return NULL;
+    }
+    recv(*sockfd, msg, (size_t) len, MSG_WAITALL);
+    msg[len] = 0; //in case half string.
     return msg;
 }
 
 int main() {
     signal(SIGINT, sigint_handler);
-    fprintf(stderr,"NW_ACLeaderBoard v0.1\n"
-           "A GPLv3-licensed Assetto Corsa Dedicated Server plugin.\n");
-
+    fprintf(stderr, "┌──────────────────────────────────────────┐\n"
+                    "│ NW_ACLeaderBoard v0.1                    │\n"
+                    "├──────────────────────────────────────────┤\n"
+                    "│ A GPLv3-licensed Assetto Corsa Dedicated │\n"
+                    "│ Server plugin for integrating with a PHP │\n"
+                    "│ Web leaderboard.                         │\n"
+                    "└──────────────────────────────────────────┘\n\n");
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -75,7 +95,7 @@ int main() {
         if(byte_count != 1) continue;
         switch(packet_id) {
             case ACSP_ERROR: {
-                wchar_t *msg = recvStringW(&sockfd, &addr);
+                wchar_t *msg = recvStringW(&sockfd);
                 if(msg) fprintf(stderr, "ACSP_ERROR: %ls\n", msg);
                 free(msg);
             }
@@ -83,28 +103,28 @@ int main() {
             case ACSP_CHAT: {
                 unsigned char car_id;
                 recvfrom(sockfd, &car_id, 1, 0, NULL, NULL);
-                wchar_t *msg = recvStringW(&sockfd, &addr);
-                if(msg) fprintf(stderr,"CAR %d: %ls\n", (int) car_id, msg);
+                wchar_t *msg = recvStringW(&sockfd);
+                if(msg) fprintf(stderr, "CAR %d: %ls\n", (int) car_id, msg);
                 free(msg);
             }
                 break;
             case ACSP_CLIENT_LOADED: {
                 unsigned char car_id;
                 recvfrom(sockfd, &car_id, 1, 0, NULL, NULL);
-                fprintf(stderr,"CAR %d loaded.\n", (int) car_id);
+                fprintf(stderr, "CAR %d loaded.\n", (int) car_id);
             }
                 break;
             case ACSP_VERSION: {
                 unsigned char proto_ver;
                 recvfrom(sockfd, &proto_ver, 1, 0, NULL, NULL);
-                fprintf(stderr,"protocol version: %d\n", (int) proto_ver);
+                fprintf(stderr, "protocol version: %d\n", (int) proto_ver);
             }
                 break;
             case ACSP_NEW_SESSION:
-                fprintf(stderr,"New session started.\n");
+                fprintf(stderr, "New session started.\n");
                 break;
             case ACSP_SESSION_INFO: {
-                fprintf(stderr,"Session Info:\n");
+                fprintf(stderr, "Session Info:\n");
                 unsigned char version, sess_index, current_session_index, session_count, type, ambient_temp, road_temp;
                 uint16_t time, laps, wait_time;
                 int32_t elapsed_ms;
@@ -117,10 +137,10 @@ int main() {
                 // The index of the current session in the server
                 recv(sockfd, &session_count, sizeof(unsigned char), MSG_WAITALL);
                 // The number of sessions in the server
-                wchar_t *server_name = recvStringW(&sockfd, &addr);
-                wchar_t *track = recvStringW(&sockfd, &addr);
-                wchar_t *track_config = recvStringW(&sockfd, &addr);
-                wchar_t *name = recvStringW(&sockfd, &addr);
+                wchar_t *server_name = recvStringW(&sockfd);
+                char *track = recvString(&sockfd);
+                char *track_config = recvString(&sockfd);
+                char *name = recvString(&sockfd);
                 recv(sockfd, &type, sizeof(unsigned char), MSG_WAITALL);
                 recv(sockfd, &session_count, sizeof(unsigned char), MSG_WAITALL);
                 // The number of sessions in the server
@@ -129,20 +149,21 @@ int main() {
                 recv(sockfd, &wait_time, sizeof(uint16_t), MSG_WAITALL);
                 recv(sockfd, &ambient_temp, sizeof(unsigned char), MSG_WAITALL);
                 recv(sockfd, &road_temp, sizeof(unsigned char), MSG_WAITALL);
-                wchar_t *weather_graphics = recvStringW(&sockfd, &addr);
+                char *weather_graphics = recvString(&sockfd);
                 recv(sockfd, &elapsed_ms, sizeof(int32_t), MSG_WAITALL);
                 // Milliseconds from the start (this might be negative for races with WaitTime)
-                fprintf(stderr,"PROTOCOL VERSION: %d\n", version);
-                fprintf(stderr,"SESSION INDEX: %d/%d\nCURRENT SESSION:%d\n", sess_index, session_count, current_session_index);
-                fprintf(stderr,"SERVER NAME: %ls\n", server_name);
-                fprintf(stderr,"TRACK: %ls\n", track);
-                fprintf(stderr,"NAME: %ls\n", name);
-                fprintf(stderr,"TYPE: %d\n", type);
-                fprintf(stderr,"TIME: %d\n", time);
-                fprintf(stderr,"LAPS: %d\n", laps);
-                fprintf(stderr,"WAIT TIME: %d\n", wait_time);
-                fprintf(stderr,"WEATHER: %ls AMBIENT %dC ROAD %dC\n", weather_graphics, ambient_temp, road_temp);
-                fprintf(stderr,"ELAPSED: %d\n", elapsed_ms);
+                fprintf(stderr, "PROTOCOL VERSION: %d\n", version);
+                fprintf(stderr, "SESSION INDEX: %d/%d\nCURRENT SESSION:%d\n", sess_index, session_count,
+                        current_session_index);
+                fprintf(stderr, "SERVER NAME: %ls\n", server_name);
+                fprintf(stderr, "TRACK: %ls\n", track);
+                fprintf(stderr, "NAME: %ls\n", name);
+                fprintf(stderr, "TYPE: %d\n", type);
+                fprintf(stderr, "TIME: %d\n", time);
+                fprintf(stderr, "LAPS: %d\n", laps);
+                fprintf(stderr, "WAIT TIME: %d\n", wait_time);
+                fprintf(stderr, "WEATHER: %ls AMBIENT %dC ROAD %dC\n", weather_graphics, ambient_temp, road_temp);
+                fprintf(stderr, "ELAPSED: %d\n", elapsed_ms);
                 free(server_name);
                 free(track);
                 free(track_config);
@@ -151,9 +172,9 @@ int main() {
             }
                 break;
             case ACSP_END_SESSION: {
-                fprintf(stderr,"ACSP_END_SESSION\n");
-                wchar_t *filename = recvStringW(&sockfd, &addr);
-                fprintf(stderr,"REPORT JSON: %ls\n", filename);
+                fprintf(stderr, "ACSP_END_SESSION\n");
+                wchar_t *filename = recvStringW(&sockfd);
+                fprintf(stderr, "REPORT JSON: %ls\n", filename);
                 free(filename);
             }
                 break;
@@ -166,36 +187,39 @@ int main() {
                     recv(sockfd, &other_car_id, sizeof(unsigned char), MSG_WAITALL);
                 recv(sockfd, &speed, sizeof(float), MSG_WAITALL);
 
-                v3f_t *world_pos = recv_v3f(&sockfd, &addr);
-                v3f_t *rel_pos = recv_v3f(&sockfd, &addr);
+                v3f_t *world_pos = recv_v3f(&sockfd);
+                v3f_t *rel_pos = recv_v3f(&sockfd);
 
                 if(ev_type == ACSP_CE_COLLISION_WITH_ENV)
-                    fprintf(stderr,"CAR: %d COLLISION WITH ENV:\n\tSPEED: %f\n\tWORLD_POS: (%f,%f,%f)\n\tREL_POS: (%f,%f,%f)\n",
-                           car_id, speed, world_pos->x, world_pos->y, world_pos->z, rel_pos->x, rel_pos->y, rel_pos->z);
+                    fprintf(stderr,
+                            "CAR: %d COLLISION WITH ENV:\n\tSPEED: %f\n\tWORLD_POS: (%f,%f,%f)\n\tREL_POS: (%f,%f,%f)\n",
+                            car_id, speed, world_pos->x, world_pos->y, world_pos->z, rel_pos->x, rel_pos->y,
+                            rel_pos->z);
                 else
-                    fprintf(stderr,"CAR: %d COLLISION WITH CAR %d:\n\tSPEED: %f\n\tWORLD_POS: (%f,%f,%f)\n\tREL_POS: (%f,%f,%f)\n",
-                           car_id, other_car_id, speed, world_pos->x, world_pos->y, world_pos->z, rel_pos->x,
-                           rel_pos->y, rel_pos->z);
+                    fprintf(stderr,
+                            "CAR: %d COLLISION WITH CAR %d:\n\tSPEED: %f\n\tWORLD_POS: (%f,%f,%f)\n\tREL_POS: (%f,%f,%f)\n",
+                            car_id, other_car_id, speed, world_pos->x, world_pos->y, world_pos->z, rel_pos->x,
+                            rel_pos->y, rel_pos->z);
                 free(world_pos);
                 free(rel_pos);
             }
                 break;
             case ACSP_CAR_INFO: {
-                fprintf(stderr,"ACSP_CAR_INFO\n");
+                fprintf(stderr, "ACSP_CAR_INFO\n");
                 unsigned char car_id, connect_status;
                 recv(sockfd, &car_id, sizeof(unsigned char), MSG_WAITALL);
                 // The server re-sends the Id so we can handle many requests at the time and still understand who we
                 // are talking about
                 recv(sockfd, &connect_status, sizeof(unsigned char), MSG_WAITALL);
-                wchar_t *model = recvStringW(&sockfd, &addr);
-                wchar_t *skin = recvStringW(&sockfd, &addr);
-                wchar_t *driver_name = recvStringW(&sockfd, &addr);
-                wchar_t *driver_team = recvStringW(&sockfd, &addr);
-                wchar_t *driver_guid = recvStringW(&sockfd, &addr);
+                wchar_t *model = recvStringW(&sockfd);
+                wchar_t *skin = recvStringW(&sockfd);
+                wchar_t *driver_name = recvStringW(&sockfd);
+                wchar_t *driver_team = recvStringW(&sockfd);
+                wchar_t *driver_guid = recvStringW(&sockfd);
 
-                fprintf(stderr,"CAR: %d\n\tModel: %ls [%ls]\n\tDriver: %ls\n\tTeam: %ls\n\tGUID: %ls\n\tSTATUS:%s\n",
-                       car_id, model, skin, driver_name, driver_team, driver_guid,
-                       (connect_status != 0 ? "Connected" : "Empty"));
+                fprintf(stderr, "CAR: %d\n\tModel: %ls [%ls]\n\tDriver: %ls\n\tTeam: %ls\n\tGUID: %ls\n\tSTATUS:%s\n",
+                        car_id, model, skin, driver_name, driver_team, driver_guid,
+                        (connect_status != 0 ? "Connected" : "Empty"));
                 free(model);
                 free(skin);
                 free(driver_name);
@@ -204,34 +228,36 @@ int main() {
             }
                 break;
             case ACSP_CAR_UPDATE: {
-                fprintf(stderr,"ACSP_CAR_UPDATE\n");
+                fprintf(stderr, "ACSP_CAR_UPDATE\n");
                 unsigned char car_id, gear;
                 uint16_t engine_rpm;
                 float normalized_spline_pos;
                 recv(sockfd, &car_id, sizeof(unsigned char), MSG_WAITALL);
-                v3f_t *pos = recv_v3f(&sockfd, &addr);
-                v3f_t *velocity = recv_v3f(&sockfd, &addr);
+                v3f_t *pos = recv_v3f(&sockfd);
+                v3f_t *velocity = recv_v3f(&sockfd);
                 recv(sockfd, &gear, sizeof(unsigned char), MSG_WAITALL);
                 recv(sockfd, &engine_rpm, sizeof(uint16_t), MSG_WAITALL);
                 recv(sockfd, &normalized_spline_pos, sizeof(float), MSG_WAITALL);
-                fprintf(stderr,"CAR %d:\n\tPos: (%f,%f,%f)\n\tVelocity: (%f,%f,%f)\n\tGear: %d\n\t Rev: %d\n\t NSP: %f\n",
-                       car_id, pos->x, pos->y, pos->z, velocity->x, velocity->y, velocity->z, gear, engine_rpm,
-                       normalized_spline_pos);
+                fprintf(stderr,
+                        "CAR %d:\n\tPos: (%f,%f,%f)\n\tVelocity: (%f,%f,%f)\n\tGear: %d\n\t Rev: %d\n\t NSP: %f\n",
+                        car_id, pos->x, pos->y, pos->z, velocity->x, velocity->y, velocity->z, gear, engine_rpm,
+                        normalized_spline_pos);
                 free(pos);
                 free(velocity);
             }
                 break;
             case ACSP_NEW_CONNECTION:
             case ACSP_CONNECTION_CLOSED: {
-                fprintf(stderr,packet_id == ACSP_CONNECTION_CLOSED ? "ACSP_CONNECTION_CLOSED\n" : "ACSP_NEW_CONNECTION\n");
-                wchar_t *driver_name = recvStringW(&sockfd, &addr);
-                wchar_t *driver_guid = recvStringW(&sockfd, &addr);
+                fprintf(stderr,
+                        packet_id == ACSP_CONNECTION_CLOSED ? "ACSP_CONNECTION_CLOSED\n" : "ACSP_NEW_CONNECTION\n");
+                wchar_t *driver_name = recvStringW(&sockfd);
+                wchar_t *driver_guid = recvStringW(&sockfd);
                 unsigned char car_id;
                 recv(sockfd, &car_id, sizeof(unsigned char), MSG_WAITALL);
-                wchar_t *car_model = recvStringW(&sockfd, &addr);
-                wchar_t *car_skin = recvStringW(&sockfd, &addr);
-                fprintf(stderr,"DRIVER: %ls GUID: %ls\n", driver_name, driver_guid);
-                fprintf(stderr,"CAR: %d MODEL: %ls %ls\n", car_id, car_model, car_skin);
+                char *car_model = recvString(&sockfd);
+                char *car_skin = recvString(&sockfd);
+                fprintf(stderr, "DRIVER: %ls GUID: %ls\n", driver_name, driver_guid);
+                fprintf(stderr, "CAR: %d MODEL: %s %s\n", car_id, car_model, car_skin);
                 free(driver_guid);
                 free(driver_name);
                 free(car_model);
@@ -239,15 +265,14 @@ int main() {
             }
                 break;
             case ACSP_LAP_COMPLETED: {
-                fprintf(stderr,"ACSP_LAP_COMPLETED\n");
+                fprintf(stderr, "ACSP_LAP_COMPLETED\n");
                 unsigned char car_id, cuts, cars_count;
                 uint32_t lap_time;
                 recv(sockfd, &car_id, sizeof(unsigned char), MSG_WAITALL);
                 recv(sockfd, &lap_time, sizeof(uint32_t), MSG_WAITALL);
                 recv(sockfd, &cuts, sizeof(unsigned char), MSG_WAITALL);
-                fprintf(stderr,"CAR: %d\n\tLAP: %d\n\tCUT: %d\n", car_id, lap_time, cuts);
+                fprintf(stderr, "CAR: %d\n\tLAP: %d\n\tCUT: %d\n", car_id, lap_time, cuts);
                 recv(sockfd, &cars_count, sizeof(unsigned char), MSG_WAITALL);
-
                 // LEADERBOARD
                 for(unsigned char i = 0; i < cars_count; i++) {
                     unsigned char rcar_id;
@@ -256,11 +281,11 @@ int main() {
                     recv(sockfd, &rcar_id, sizeof(rcar_id), MSG_WAITALL);
                     recv(sockfd, &rtime, sizeof(rtime), MSG_WAITALL);
                     recv(sockfd, &rlaps, sizeof(rlaps), MSG_WAITALL);
-                    fprintf(stderr,"%d: CAR_ID: %d TIME: %d LAPS: %d\n", i, rcar_id, rtime, rlaps);
+                    fprintf(stderr, "%d: CAR_ID: %d TIME: %d LAPS: %d\n", i, rcar_id, rtime, rlaps);
                 }
                 float grip_level;
                 recv(sockfd, &grip_level, sizeof(grip_level), MSG_WAITALL);
-                fprintf(stderr,"GRIP LEVEL: %f\n", grip_level);
+                fprintf(stderr, "GRIP LEVEL: %f\n", grip_level);
             }
                 break;
             default:
